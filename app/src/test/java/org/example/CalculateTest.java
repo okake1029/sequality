10adb62ab8590e0333213e1321825da29445c14e
okada keisuke <okakei2910@gmai.com>
2024-07-01 21:30:55 +0900

    kadai2-3

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -21,7 +21,7 @@ public class CalculateTest {
   }
 
   @Test
-  public void allSum() {
+  public void testAllSum() {
     Calculate calculate = new Calculate();
     int expected = 55;
     assertEquals(expected, calculate.allSum(1, 10), 0.01);
@@ -35,14 +35,14 @@ public class CalculateTest {
   }
 
   @Test
-  public void oddSum() {
+  public void testOddSum() {
     Calculate calculate = new Calculate();
     int expected = 25;
     assertEquals(expected, calculate.oddSum(1, 10), 0.01);
   }
 
   @Test
-  public void evenSum() {
+  public void testEvenSum() {
     Calculate calculate = new Calculate();
     int expected = 30;
     assertEquals(expected, calculate.evenSum(1, 10), 0.01);
